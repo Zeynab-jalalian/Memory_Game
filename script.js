016749d913bd3fca1ea1fdaf2c468d53a370e55d
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-22 09:50:55 +0330

    part1

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const startPage=document.getElementById("start-page");
 const startBtn=document.getElementById("start-btn");
+
+startBtn.addEventListener("click",()=>{
+   setTimeout(() => {
+     startPage.style.display="none";
+   }, 100);
+
+})
