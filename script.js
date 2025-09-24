c42a48f0f670d1d0a2ea98cd56faab8748166829
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-24 10:48:08 +0330

    js category

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const time = document.querySelector(".time");
 const catPage = document.querySelector(".category-page");
 const CatBtns = document.querySelectorAll(".cat-btn");
 const Art = document.querySelector(".art");
+const show = document.querySelector(".show");
 const wrapper = document.querySelector(".wrapper");
 const gameContainer = document.querySelector(".games-container");
 
@@ -20,7 +21,9 @@ let firstCard = false;
 let secondCard = false;
 
 let interval;
+let currentItems = [];
 
+// Art items
 const items = [
   { name: "art1", image: "images/art1.jpg" },
   { name: "art2", image: "images/art2.jpg" },
@@ -36,142 +39,146 @@ const items = [
   { name: "art12", image: "images/art12.jpg" },
 ];
 
-//pick random cards
+// Film items
+const items2 = [
+  { name: "film1", image: "images/film1.jpg" },
+  { name: "film2", image: "images/film2.jpg" },
+  { name: "film3", image: "images/film3.jpg" },
+  { name: "film4", image: "images/film4.jpg" },
+  { name: "film5", image: "images/film5.jpg" },
+  { name: "film6", image: "images/film6.jpg" },
+  { name: "film7", image: "images/film7.jpg" },
+  { name: "film8", image: "images/film8.jpg" },
+  { name: "film9", image: "images/film9.jpg" },
+  { name: "film10", image: "images/film10.jpg" },
+  { name: "film11", image: "images/film11.jpg" },
+  { name: "film12", image: "images/film12.jpg" },
+];
+
+// Pick random cards
 const randomCards = (size = 4) => {
-  let tempArray = [...items];
+  let tempArray = [...currentItems];
   let cardValues = [];
   size = (size * size) / 2;
   for (let i = 0; i < size; i++) {
-    randomIndex = Math.floor(Math.random() * tempArray.length);
+    let randomIndex = Math.floor(Math.random() * tempArray.length);
     cardValues.push(tempArray[randomIndex]);
     tempArray.splice(randomIndex, 1);
   }
   return cardValues;
 };
-//shuffle
+
+// Shuffle & create game board
 const matrixGenerator = (cardValues, size = 4) => {
   gameContainer.innerHTML = "";
   cardValues = [...cardValues, ...cardValues];
   cardValues.sort(() => Math.random() - 0.5);
+
   for (let i = 0; i < size * size; i++) {
     gameContainer.innerHTML += `
-     <div class="card-container" data-card-value="${cardValues[i].name}">
-     <div class="card-before">?</div>
-     <div class="card-after">
-     <img src="${cardValues[i].image}" class="image"></div>
-     </div>
-     </div>
-     `;
-    cards = document.querySelectorAll(".card-container");
-    cards.forEach((card) => {
-      card.addEventListener("click", () => {
-        const dingSound2 = new Audio("sounds/ding2.mp3");
-        dingSound2.play();
-        if (!card.classList.contains("matched")) {
-          card.classList.add("flipped");
-          if (!firstCard) {
-            firstCard = card;
-            firstCardValue = card.getAttribute("data-card-value");
-          } else {
-            MovesCounter();
-            secondCard = card;
-            let secondCardValue = card.getAttribute("data-card-value");
-            if (firstCardValue == secondCardValue) {
-              const dingSound = new Audio("sounds/ding1.mp3");
-              dingSound.play();
-
-              firstCard.classList.add("matched");
-              secondCard.classList.add("matched");
-              firstCard = false;
-              secondCard = false;
-              winCount += 1;
-              if (winCount == Math.floor(cardValues.length / 2)) {
-                const dingSound3 = new Audio("sounds/ding3.mp3");
-                dingSound3.play();
-                result.innerHTML = `
-        <h2>Well Done!</h2>
-        <h3>Moves: ${move}</h3>
+      <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-before">?</div>
+        <div class="card-after">
+          <img src="${cardValues[i].image}" class="image">
+        </div>
+      </div>
     `;
+  }
 
-                // رفتن به صفحه استارت
-                startPage.classList.remove("hide");
-                wrapper.classList.add("hide");
-                catPage.classList.remove("hide");
+  cards = document.querySelectorAll(".card-container");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const dingSound2 = new Audio("sounds/ding2.mp3");
+      dingSound2.play();
 
-                clearInterval(interval);
-              }
-            } else {
-              let [tempFirst, tempSecond] = [firstCard, secondCard];
-              firstCard = false;
-              secondCard = false;
+      if (!card.classList.contains("matched")) {
+        card.classList.add("flipped");
 
-              let delay = setTimeout(() => {
-                tempFirst.classList.remove("flipped");
-                tempSecond.classList.remove("flipped");
-              }, 400);
+        if (!firstCard) {
+          firstCard = card;
+          firstCardValue = card.getAttribute("data-card-value");
+        } else {
+          MovesCounter();
+          secondCard = card;
+          let secondCardValue = card.getAttribute("data-card-value");
+
+          if (firstCardValue == secondCardValue) {
+            const dingSound = new Audio("sounds/ding1.mp3");
+            dingSound.play();
+
+            firstCard.classList.add("matched");
+            secondCard.classList.add("matched");
+            firstCard = false;
+            secondCard = false;
+            winCount += 1;
+
+            if (winCount == Math.floor(cardValues.length / 2)) {
+              const dingSound3 = new Audio("sounds/ding3.mp3");
+              dingSound3.play();
+              result.innerHTML = `
+                <h2>Well Done!</h2>
+                <h3>Moves: ${move}</h3>
+              `;
+
+              startPage.classList.remove("hide");
+              wrapper.classList.add("hide");
+              catPage.classList.remove("hide");
+              clearInterval(interval);
             }
+          } else {
+            let [tempFirst, tempSecond] = [firstCard, secondCard];
+            firstCard = false;
+            secondCard = false;
+            setTimeout(() => {
+              tempFirst.classList.remove("flipped");
+              tempSecond.classList.remove("flipped");
+            }, 400);
           }
         }
-      });
+      }
     });
-  }
+  });
 };
+
+// Start Button
 startBtn.addEventListener("click", () => {
   const clickSound = new Audio("sounds/click.mp3");
   clickSound.play();
-  setTimeout(() => {
-    startPage.classList.add("hide");
-  }, 100);
+  startPage.classList.add("hide");
+  catPage.classList.remove("hide");
 });
 
-const MovesCounter = () => {
-  move += 1;
-  moves.innerHTML = `<span>Moves</span>:${move}`;
-};
-const timeLimit = () => {
-  seconds -= 1;
-  if (seconds <= 0) {
-    seconds = 0;
-    stopGame();
-    const oops = new Audio("sounds/oops.mp3");
-    oops.play();
-    result.innerHTML = `
-        <h2>Game Over!</h2>
-     
-    `;
-
-    startPage.classList.remove("hide");
-    wrapper.classList.add("hide");
-    catPage.classList.remove("hide");
-  }
-  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
-  time.innerHTML = `<span>Time</span>:${secondsValue}`;
-};
-
-Art.addEventListener("click", () => {
+// Start game function
+function startGame(itemsArray) {
   const clickSound = new Audio("sounds/click.mp3");
   clickSound.play();
-    // پاک کردن اینتروال قبلی اگر وجود داشته باشه
-    clearInterval(interval);
 
-    // ریست متغیرها
-    move = 0;
-    winCount = 0;
-    seconds = 60;
-    moves.innerHTML = `<span>Moves</span>:${move}`;
-    time.innerHTML = `<span>Time</span>:${seconds}`;
+  clearInterval(interval);
+  currentItems = itemsArray;
+  move = 0;
+  winCount = 0;
+  seconds = 60;
+  moves.innerHTML = `<span>Moves</span>:${move}`;
+  time.innerHTML = `<span>Time</span>:${seconds}`;
+  result.innerText = "";
 
-    // صفحات
-    catPage.classList.add("hide");
-    wrapper.classList.remove("hide");
+  catPage.classList.add("hide");
+  wrapper.classList.remove("hide");
 
-    // شروع تایمر
-    interval = setInterval(timeLimit, 1000);
+  interval = setInterval(timeLimit, 1000);
+  initializer();
+}
 
-    // شروع بازی
-    initializer();
+// Category buttons
+Art.addEventListener("click", () => {
+  startGame(items);
 });
 
+show.addEventListener("click", () => {
+  startGame(items2);
+});
+
+// Stop game
 function stopGame() {
   const clickSound = new Audio("sounds/click.mp3");
   clickSound.play();
@@ -183,13 +190,33 @@ function stopGame() {
   clearInterval(interval);
   seconds = 60;
   time.innerHTML = `<span>Time</span>: ${seconds}`;
-
   move = 0;
   moves.innerHTML = `<span>Moves</span>: ${move}`;
 }
 
 stopBtn.addEventListener("click", stopGame);
 
+// Moves counter
+const MovesCounter = () => {
+  move += 1;
+  moves.innerHTML = `<span>Moves</span>:${move}`;
+};
+
+// Timer
+const timeLimit = () => {
+  seconds -= 1;
+  if (seconds <= 0) {
+    seconds = 0;
+    stopGame();
+    const oops = new Audio("sounds/oops.mp3");
+    oops.play();
+    result.innerHTML = `<h2>Game Over!</h2>`;
+  }
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  time.innerHTML = `<span>Time</span>:${secondsValue}`;
+};
+
+// Initialize game
 const initializer = () => {
   result.innerText = "";
   winCount = 0;
